26da5d8f150c5b65c143f433fba00e694dbcc443
Dmitrii <d9619938@yandex.ru>
2023-10-01 15:26:39 +0300

    Первый коммит: Созданы три класс фаила File01.java, File02.java, File03.java

---
+++ src/File02.java
@@ -0,0 +1,5 @@
+public class File02 {
+    public static void main(String[] args) {
+        System.out.println("File02");
+    }
+}
---
+++ src/File03.java
@@ -0,0 +1,5 @@
+public class File03 {
+    public static void main (String[] args) {
+        System.out.println("File03");
+    }
+}
